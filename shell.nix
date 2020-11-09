7eb34b788682762a41743c82529cc54f0fee03f5
Jechol Lee <mr.jechol@gmail.com>
2020-11-09 22:40:22 +0900

    Add compile tools

--- shell.nix
+++ shell.nix
@@ -13,5 +13,14 @@ in nixpkgs.mkShell {
     beam.pkg.v23_0.elixir.v1_11_0
     beam.pkg.v23_0.rebar3
     beam.pkg.v23_0.rebar
+
+    # build tools required for hex pkgs (ex. libsecp256k1, keccakf1600)
+    nixpkgs.libtool
+    nixpkgs.autoconf
+    nixpkgs.automake
+    nixpkgs.gcc
+    nixpkgs.darwin.apple_sdk.frameworks.CoreServices
+    nixpkgs.gmp6
+    nixpkgs.libiconv
   ];
 }
